e42c760aa65f4f4273de4c6af86f2856697060e4
MrSpock222 <niki.ruedi@gmail.com>
2025-11-23 01:57:07 +0100

    Update Uebung4.py

--- Semester_1/Uebungen/Uebung4.py
+++ Semester_1/Uebungen/Uebung4.py
@@ -1,35 +1,124 @@
 #Uebung 1
 def ticktactoe():
-    reihe1 = []
-    reihe2 = []
-    reihe3 = []
+    reihe1 = ["*", "*", "*"]
+    reihe2 = ["*", "*", "*"]
+    reihe3 = ["*", "*", "*"]
+    print(reihe1)
+    print(reihe2)
+    print(reihe3)
     
-    a = input("\nWelche Reihe?")
-    b = input("Welche Spalte?")
+    Spieler1 = True
+    Gewonnen = False
     
-    n = "X"
-    
-    if a == "1": 
-        if b == "1":
-            n = "X" if n == "O" else "O"
-            reihe1.insert(0, n)
+    while not Gewonnen:
+        if Spieler1 == True and Gewonnen == False:
+            a = int(input("\nSpieler 1 Welche Reihe?"))
+            b = int(input("\nSpieler 1 Welche Spalte?"))
+            if a == 1:
+                reihe1[b-1] = "X"
+            if a == 2:
+                reihe2[b-1] = "X"
+            if a == 3:
+                reihe3[b-1] = "X"
+            Spieler1 = False
         print(reihe1)
-        
-        
-    if a == "2":
-        if b == "2":
-            n = "X" if n == "O" else "O"
-            reihe2.insert(1, n)
-
         print(reihe2)
-    if a == "3":
-        if b == "3":
-            n = "X" if n == "O" else "O"
-            reihe3.insert(2, n)
         print(reihe3)
-
-
-    
-    
-    
+        for i in range(0,3):
+            if reihe1[i] == reihe2[i] == reihe3[i] == "X":
+                print("\nSpieler 1 gewinnt!")
+                Gewonnen = True
+            if reihe1[i] == reihe2[i] == reihe3[i] == "O":
+                print("\nSpieler 2 gewinnt!")
+                Gewonnen = True
+        if reihe1[0] == "X" and reihe1[1] == "X" and reihe1[2] == "X":
+            print("\nSpieler 1 gewinnt!")
+            Gewonnen = True
+        if reihe1[0] == "O" and reihe1[1] == "O" and reihe1[2] == "O":
+            print("\nSpieler 2 gewinnt!")
+            Gewonnen = True
+        if reihe2[0] == "X" and reihe2[1] == "X" and reihe2[2] == "X":
+            print("\nSpieler 1 gewinnt!")
+            Gewonnen = True
+        if reihe2[0] == "O" and reihe2[1] == "O" and reihe2[2] == "O":
+            print("\nSpieler 2 gewinnt!")
+            Gewonnen = True
+        if reihe3[0] == "X" and reihe3[1] == "X" and reihe3[2] == "X":
+            print("\nSpieler 1 gewinnt!")
+            Gewonnen = True
+        if reihe3[0] == "O" and reihe3[1] == "O" and reihe3[2] == "O":
+            print("\nSpieler 2 gewinnt!")
+            Gewonnen = True
+        if reihe1[0] == "X" and reihe2[1] == "X" and reihe3[2] == "X":
+            print("\nSpieler 1 gewinnt!")
+            Gewonnen = True
+        if reihe1[0] == "O" and reihe2[1] == "O" and reihe3[2] == "O":
+            print("\nSpieler 2 gewinnt!")
+            Gewonnen = True
+        if "*" not in reihe1 and "*" not in reihe2 and "*" not in reihe3:
+            print("\nUnentschieden!")
+            Gewonnen = True
+            
+        if Spieler1 == False and Gewonnen == False:
+            a = int(input("\nSpieler 2 Welche Reihe?"))
+            b = int(input("\nSpieler 2 Welche Spalte?"))
+            if a == 1:
+                reihe1[b-1] = "O"
+            if a == 2:
+                reihe2[b-1] = "O"
+            if a == 3:
+                reihe3[b-1] = "O"
+            Spieler1 = True
+        print(reihe1)
+        print(reihe2)
+        print(reihe3)
+        for i in range(0,3):
+            if reihe1[i] == reihe2[i] == reihe3[i] == "X":
+                print("\nSpieler 1 gewinnt!")
+                Gewonnen = True
+            if reihe1[i] == reihe2[i] == reihe3[i] == "O":
+                print("\nSpieler 2 gewinnt!")
+                Gewonnen = True
+        if reihe1[0] == "X" and reihe1[1] == "X" and reihe1[2] == "X":
+            print("\nSpieler 1 gewinnt!")
+            Gewonnen = True
+        if reihe1[0] == "O" and reihe1[1] == "O" and reihe1[2] == "O":
+            print("\nSpieler 2 gewinnt!")
+            Gewonnen = True
+        if reihe2[0] == "X" and reihe2[1] == "X" and reihe2[2] == "X":
+            print("\nSpieler 1 gewinnt!")
+            Gewonnen = True
+        if reihe2[0] == "O" and reihe2[1] == "O" and reihe2[2] == "O":
+            print("\nSpieler 2 gewinnt!")
+            Gewonnen = True
+        if reihe3[0] == "X" and reihe3[1] == "X" and reihe3[2] == "X":
+            print("\nSpieler 1 gewinnt!")
+            Gewonnen = True
+        if reihe3[0] == "O" and reihe3[1] == "O" and reihe3[2] == "O":
+            print("\nSpieler 2 gewinnt!")
+            Gewonnen = True
+        if reihe1[0] == "X" and reihe2[1] == "X" and reihe3[2] == "X":
+            print("\nSpieler 1 gewinnt!")
+            Gewonnen = True
+        if reihe1[0] == "O" and reihe2[1] == "O" and reihe3[2] == "O":
+            print("\nSpieler 2 gewinnt!")
+            Gewonnen = True
+        
+        if "*" not in reihe1 and "*" not in reihe2 and "*" not in reihe3:
+            print("\nUnentschieden!")
+            Gewonnen = True
 ticktactoe()
+
+        
+            
+        
+        
+    
+        
+           
+    
+    
+
+
+    
+    
