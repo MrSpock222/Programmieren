28ba4ca4462dd424c3ffdbd0770090b59a2a683a
MrSpock222 <niki.ruedi@gmail.com>
2025-12-03 22:37:24 +0100

    031225

--- Semester_1/Uebungen/Uebung5_1.py
+++ Semester_1/Uebungen/Uebung5_1.py
@@ -1,2 +1,11 @@
-def exponential(z,N):
-    
+def ez(z, N):
+	s = 0.0
+	fact = 1  # k!
+	pwr = 1.0  # z**k
+	for k in range(N):
+		if k > 0:
+			fact *= k
+			pwr *= z
+		s += pwr / fact
+	return s
+print(ez(2, 10))
